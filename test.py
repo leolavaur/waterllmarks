a2065b7a2fad267dd66ca046064fe5fddf2d2b8e
Léo Lavaur <82591009+leolavaur@users.noreply.github.com>
2025-01-08 13:31:44 +0100

    Project setup.

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-from openai import OpenAI
 import os
 import pathlib
 
+from openai import OpenAI
+
 with open(pathlib.Path(__file__).parent / ".env", "r") as f:
-    os.environ.update(dict(
-        line.strip().split("=") for line in f if not line.startswith("#")
-    ))
+    os.environ.update(
+        dict(line.strip().split("=") for line in f if not line.startswith("#"))
+    )
 
 client = OpenAI()
 
 completion = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"},
+    ],
 )
 print(completion.choices[0].message)
